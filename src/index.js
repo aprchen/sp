7ed3f4588f18f9018fcb070dbb2005582e860fc5
ott321 <ott321@yeah.net>
2018-01-17 22:51:05 +0800

    页面获取成功

--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ import { log } from 'util';
 
 const parser = require('./parser.js');
 const request = require('./request.js');
+const fs = require('fs');
 let baseUrl = 'http://guofeng.yuedu.163.com/';
 let bookUrl = baseUrl + 'source/ts_520094e47a4e4fdb8b93d8889bd67e38_4/';
+let t = Date.now();
+//let html = request.getEl("http://guofeng.yuedu.163.com/book_reader/ts_520094e47a4e4fdb8b93d8889bd67e38_4/bc12916d599342b3a04e24f3f66e0732_4/");
+let html = request.getEl("http://guofeng.yuedu.163.com/source/ts_520094e47a4e4fdb8b93d8889bd67e38_4/");
+html.then(res => {
+    fs.appendFile('book.txt', res, 'utf-8', function (err) {
+        if (err) throw err;
+        else console.log('html写入成功')
+        t = Date.now() - t;
+        console.log('Loading time ' + t + ' msec');
+    });
+}).catch(error => {
+    log(error);
+});
 
-request.getPic("http://www.baidu.com/")
-// let html = '';
-// html = request('GET', bookUrl).getBody().toString();
-// if (html) console.log("get html success");
-// parser.bookHandle(html);
+// if (html) {
+//     fs.appendFile('html.txt', html, 'utf-8', function (err) {
+//         if (err) throw err;
+//         else console.log('html信息写入成功' + '\r\n');
+//     });
+// }
+
--- src/request.js
+++ src/request.js
@@ -3,7 +3,7 @@ import { log } from 'util';
 'use strict'
 const phantom = require('phantom');
 export async function getPic(url) {
-
+    let t = Date.now();
     // const instance = await phantom.create([], {
     //     logLevel: 'debug',
     // });//创建一个实例
@@ -16,11 +16,11 @@ export async function getPic(url) {
     //设置页面参数
     let width = 1800;
     let height = 1200;
-    await page.property('viewportSize', { width:`${width}`, height: `${height}` });
+    await page.property('viewportSize', { width: `${width}`, height: `${height}` });
     log(`页面大小:  ${width} *  ${height}\n`);
     let enabled = true;
-    await page.property('javascriptEnabled', {value:`${enabled}`});
-    await page.property('setTimeout', {value:5000});
+    await page.property('javascriptEnabled', { value: `${enabled}` });
+    await page.property('setTimeout', { value: 5000 });
 
     log(`js 加载:  ${enabled} \n`);
     // const content = await page.property('content');
@@ -36,5 +36,41 @@ export async function getPic(url) {
     log('销毁实例 \n');
     await instance.exit();
     //返回数据
+
     return content;
 };
+
+
+export async function getEl(url) {
+    const instance = await phantom.create(['--ignore-ssl-errors=yes', '--load-images=no']);//创建一个实例
+    log('创建实例 \n');
+    const page = await instance.createPage();
+    let urls=[];
+    await page.on('onResourceRequested',true, function (requestData, request) {
+        log(`请求中... ${requestData.url}\n`);
+        // let = regex = '/http(s)?:\/\/.+?\.css$/gi';
+        // let re = new RegExp(regex, 'ig');
+        // let isBlacklisted = re.test(requestData.url);
+        if ((/http(s)?:\/\/.+?\.css$/gi).test(requestData.url)) {
+            console.log('Skipping', requestData.url);
+            urls.push(requestData.url);
+            request.abort();
+        }
+    });
+    //设置页面参数
+    // let width = 800;
+    // let height = 600;
+    // await page.property('viewportSize', { width: `${width}`, height: `${height}` });
+    // log(`页面大小:  ${width} *  ${height}\n`);
+
+    //WebKit/534.46 Mobile/9A405 Safari/7534.48.3
+    await page.property('userAgent', "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) Gecko/20100101 Firefox/53.0");
+    //打开url，返回状态（url有转码，解决中文问题）
+    const status = await page.open(encodeURI(url));
+    const content = await page.property('content');
+    log(`请求状态:  ${status} \n`);
+    await instance.exit();
+    log('销毁实例 \n');
+    log(urls);
+    return content;
+}
